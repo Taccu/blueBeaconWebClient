c85831cc0e0dc70ec4216b3cfe3a2e3f62b94080
zoellerd <zoellerd@dhbw-loerrach.de>
2016-09-20 21:40:17 +0200

    JSON API: return mapping as well

--- json.php
+++ json.php
@@ -3,24 +3,32 @@ require("model/maschine.php");
 require("model/beacon.php");
 require("controller/database.php");
 
-$test_arr = array(
+$return_arr = array(
 	"machines" => array(),
 	"beacons" => array()
 );
 
 $machine_1 = new Maschine();
-	$beacon_1 = new Beacon();
-	
-	$result_1=$beacon_1->getjsondata();
-	$result_2=$machine_1->getjsonData();
-	while ($object = $result_1->fetch(PDO::FETCH_OBJ)) 
-	{
-		$test_arr["beacons"][] = $object;
-	}
-	while ($object = $result_2->fetch(PDO::FETCH_OBJ)) 
-	{
-		$test_arr["machines"][] = $object;
-	}
-	
-	echo json_encode($test_arr);
-	
+$beacon_1 = new Beacon();
+
+$result_1 = $beacon_1->getjsondata();
+$result_2 = $machine_1->getjsonData();
+while($object = $result_1->fetch(PDO::FETCH_OBJ)) 
+{
+	$return_arr["beacons"][] = $object;
+}
+while($object = $result_2->fetch(PDO::FETCH_OBJ)) 
+{
+	$return_arr["machines"][] = $object;
+}
+$pdo = $this->db->connection();
+$sStmt = "SELECT * FROM bb_mapping";
+$db_query = $pdo->query($sStmt);
+while($object = $db_query->fetch(PDO:FETCH_OBJ))
+{
+	$return_arr["mapping"][] = $object;
+}
+$pdo=null;
+
+echo json_encode($return_arr);
+
